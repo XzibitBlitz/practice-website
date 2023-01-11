03f5462c19f107e572fe85f52deabe7a5b9915d6
Jaska <50119926+XzibitBlitz@users.noreply.github.com>
2023-01-11 15:32:36 -0800

    Second Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import "@fontsource/dm-sans"
-
-const inter = Inter({ subsets: ['latin'] })
+import "@fontsource/dm-sans";
 
 export default function Home() {
   return (
